b0bea04e5f1ac7b63f7e82787bc6762d349bdd32
caiwei.lin <caiwei.lin@bekencorp.com>
2018-06-22 15:43:08 +0800

    creat file structure

---
+++ sl_code/platform/mcu/mcu_platform.h
@@ -0,0 +1,13 @@
+#ifndef _MCU_PLATFORM_H_
+#define _MCU_PLATFORM_H_
+
+
+#ifdef STM32F10x_PLATFROM
+#include "stdio.h"
+#include "string.h"
+#include "stdbool.h"
+
+#endif
+
+
+#endif
---
+++ sl_code/platform/mcu/stm32f103x/hal_entry/main.c
@@ -0,0 +1,13 @@
+#include "mcu_platform.h"
+
+
+int main(void)
+{
+    while(1)
+    {
+			
+    }
+}
+
+
+/***********************  END OF FILES  ***********************/
